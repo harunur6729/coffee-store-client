35272a39e73e0ae3d6f380d523556e400fad2129
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-03-10 16:28:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-red-800'>Vite + React</h1>
+      <button className='btn'> X</button>
+      
+      
+    </>
+  )
+}
+
+export default App
