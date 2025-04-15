87d9fa94c1a95700b47c4d4fc9b697783bf3cf60
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-04-15 12:00:46 +0600

    delete added

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,10 +1,12 @@
+import { data } from "autoprefixer";
 import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const {_id, name, quantity, supplier, taste, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
 
-  const handleDelete = _id =>{
-    console.log(_id)
+  const handleDelete = (_id) => {
+    console.log(_id);
 
     Swal.fire({
       title: "Are you sure?",
@@ -13,17 +15,27 @@ const CoffeeCard = ({ coffee }) => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success"
-        });
+        console.log("delete confirmed");
+        fetch(`http://localhost:5000/coffee/${_id}`,{
+          method:'delete',
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deleteCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success"
+              });
+            }
+          });
       }
     });
-  }
+  };
   return (
     <div className="grid ">
       <div className="card card-side bg-indigo-900 shadow-md shadow-red-300">
@@ -44,7 +56,12 @@ const CoffeeCard = ({ coffee }) => {
           <div className="join join-vertical justify-evenly  p-2 ">
             <button className="btn btn-secondary join-item">view</button>
             <button className="btn btn-success join-item">Edit</button>
-            <button onClick={()=>handleDelete(_id)} className="btn btn-info join-item">Delete</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn btn-info join-item"
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
